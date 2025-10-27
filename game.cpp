b4ac3034d9b95f988ef75918e9b2f8c00c8dadc7
sll00 <1275142234@qq.com>
2025-10-27 17:19:20 +0800

    设想

--- game.cpp
+++ game.cpp
@@ -48,13 +48,11 @@ public:
     }
 
 };
-class Chessboard   : Photo{
+class Chessboard {
     int width,height;   //宽高
-    int Photo_num;    //图片编号
-    string Path;          //图片地址
     bool status;         //棋盘状态  
 public:
-    Chessboard(int _width, int _height, int _Photo_num, string  _Path ,bool _status) :width(_width), height(_height), Photo_num(_Photo_num), Path(_Path), status(_status) {};
+    Chessboard(int _width, int _height ,bool _status) :width(_width), height(_height), status(_status) {};
     void init(int width,int height){//初始化
 
     }
@@ -64,7 +62,6 @@ public:
     void unban(){//开盘
 
     }
-
 };
 
 class Piece{
@@ -73,14 +70,39 @@ class Piece{
     bool highlight;     //是否为高光
 public :
     Piece(string _c,int _x, int _y,int _id, bool _h) : color(_c),x(_x),y(_y),id(_id),highlight(_h) {};
-        
+    void inplace(){//放置
+
+    }
+    bool check_xy(){//是否越界
+
+    }
+    bool check_win(){//是否导致胜局
+
+    }
+    bool set_key(){//设置为高光
+
+    }
+    bool unset_key(){//取消高光
+
+    }
+
 };
 
 class Gamer{
     std :: string name,ID;  //姓名，玩家ID
     int rank,done_num;    //段位，已经下过步数
+    string belongto;         //阵营
 public:
-    Gamer(string _name, string _ID, int _rank, int _done_num) : name(_name), ID(_ID) ,rank(_rank), done_num(_done_num) {};
+    Gamer(string _name, string _ID, int _rank, int _done_num, string _belongto) : name(_name), ID(_ID) ,rank(_rank), done_num(_done_num), belongto(_belongto) {};
+    void login(){//登录
+
+    }
+    void logout(){//退出
+
+    }
+    void change(){//改变阵营
+
+    }
 };
 
 int main()
