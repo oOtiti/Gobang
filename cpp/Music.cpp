ec5f92ef0fb7ccb153289c7f43aef69c5ac2f650
sll00 <1275142234@qq.com>
2025-10-30 17:59:56 +0800

    游戏音乐

--- cpp/Music.cpp
+++ cpp/Music.cpp
@@ -1,31 +1,68 @@
 #include "Music.h"
+#include <windows.h>
 #include <mmsystem.h>
+#include <iostream>
 #pragma comment(lib, "winmm.lib")
 
 Music :: Music(std :: string _name ,std ::string _Path ,int _Music_num, bool _status, int _set0, bool _loop,
-std :: vector <std ::string> _Path_arr , int _tot)
-    :name(_name), Path(_Path) , Music_num(_Music_num) , status(_status), set0(_set0), loop(_loop) ,Path_arr(_Path_arr), tot(_tot){ };
+std :: vector <std ::string> _Path_arr , int _tot ,int _cur)
+    :name(_name), Path(_Path) , Music_num(_Music_num) , status(_status), set0(_set0), loop(_loop) ,Path_arr(_Path_arr), tot(_tot), cur(_cur){ };
 
-void Music :: turn_off () {
-
+void Music :: turn_on ( const std :: string& path ,const int& to) {
+    std::string openCmd = "open \"" + path + "\" alias " + this->name;
+    std::string playCmd = "play " + this->name;
+    MCIERROR err=mciSendStringA(openCmd.c_str(),NULL, 0 ,NULL);
+    if(err) {std ::cout<<"open path error!"<<std :: endl;return;}
+    this->cur=to;
+    this->status=true;
+    mciSendStringA(playCmd.c_str(),NULL ,0,NULL);
 }
 
-void Music :: turn_on () {
-
+void Music :: turn_off () {
+    std::string pauseCmd = "pause " + this->name;
+    mciSendStringA(pauseCmd.c_str(),NULL ,0,NULL);
 }
 
 void Music :: last () {
-
+    int need=((*this).cur-1+(*this).tot)%(*this).tot;
+    this->turn_on(this->Path_arr[need],need);
 }
 
 void Music :: next () {
-
+    int need=((*this).cur+1)%(*this).tot;
+    this->turn_on(this->Path_arr[need],need);
 }
 
-void Music :: change_to () {
-
+void Music :: change_to (const int& to) {
+    if(to<0||to>this->tot){std :: cout<< "ERROR : NO MUSIC!"<<std ::endl;}
+    this->turn_on(this->Path_arr[to],to);
 }
 
-void put_music(const std :: string& path){
-    
+void Music :: change_set0 (const int& num) {
+    if(num>tot||num<0) {std ::cout<< "ERROR : NO MUSIC!"<< std ::endl;return;}
+    (*this).set0 =num;
 }
+
+void Music :: push_music (const std :: string& path) {
+    (*this).Path_arr.push_back(path);
+    (*this).tot++;
+    std :: cout<<"OK, has update a music !"<<std :: endl;
+}
+
+void Music :: pop_music () {
+    if((*this).Path_arr.empty()) {std :: cout << "ERROR : NO MUSIC!"<<std :: endl;return;}
+    (*this).Path_arr.pop_back();
+    (*this).tot--;
+}
+void Music :: show () const {
+    if((*this).Path_arr.empty()){std :: cout << "NO MUSCI!"<<std :: endl; }
+    else
+    {
+            std :: cout<<"---------------------MUSIC TABLE---------------------" << std :: endl;
+        for(int i=0; i<=(*this).tot; i++)
+            std :: cout << i+1<< ". is "<< (*this).Path_arr[i]<<std :: endl;
+    }
+    std :: cout<< "music now is"<<((*this).status?"open":"close")<< std ::endl;
+    std :: cout<< "set0 is"<<(*this).set0 <<std ::endl;
+    if((*this).status) std :: cout<<"Now is playing" << (*this).name<<std ::endl;
+} 
--- hpp/Music.h
+++ hpp/Music.h
@@ -10,20 +10,22 @@ private:
     std :: string Path;                                   //音乐路径
     int Music_num;                                     //音乐编号
     bool status;                                           //音乐状态
-    int set0;                                                 //默认音乐
-    bool loop;                                             //循环
-    std :: vector<std ::string> Path_arr;      //存路径
+    int set0;                                                //默认音乐
+    bool loop;                                            //循环
+    std :: vector<std ::string> Path_arr;     //存路径
     int tot;                                                  //总歌曲数
+    int cur;                                                 //当前
 public:
     Music (std :: string _name="RAP" ,std :: string _Path=" ", int _Music_num=0, bool _status=0, int _set0=0,bool _loop=false,
-    std ::vector<std :: string> _Path_arr ,int _tot);
-    void put_music(const std :: string& path); //添加音乐
-    void show();                    //展示现在的音乐
-    void turn_on();               //打开音乐
-    void turn_off();              //关闭音乐
-    void last();                     //切换上一首
+    std ::vector<std :: string> _Path_arr ,int _tot=0, int _cur=0);
+    void push_music(const std :: string&); //添加音乐
+    void pop_music();        //删除音乐
+    void show() const;       //展示现在的音乐
+    void turn_off();             //关闭音乐
+    void last();                    //切换上一首
     void next();                   //切换下一首
-    void change_to();         //切换到index编号音乐
-    void make_loop();        //循环
+    void change_to(const int&);         //切换到index编号音乐
+    void change_set0(const int&);      //改变默认音乐
+    void turn_on(const std :: string& path ,const int&) ;             //打开音乐
 };
 #endif
